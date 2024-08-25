cfb02a47ae878fa7e2529a7deb4aef8eb776dabe
Johannes Hostert <jhostert@ethz.ch>
2024-08-25 19:09:53 +0200

    Add miri version printer

--- build.rs
+++ build.rs
@@ -1,3 +1,26 @@
+fn get_git_hash() -> String {
+    use std::process::Command;
+
+    let branch = Command::new("git").arg("rev-parse").arg("--abbrev-ref").arg("HEAD").output();
+    if let Ok(branch_output) = branch {
+        let branch_string = String::from_utf8_lossy(&branch_output.stdout);
+        let commit = Command::new("git").arg("rev-parse").arg("--verify").arg("HEAD").output();
+        if let Ok(commit_output) = commit {
+            let commit_string = String::from_utf8_lossy(&commit_output.stdout);
+
+            format!(
+                "branch {}, hash {}",
+                branch_string.lines().next().unwrap_or("?"),
+                commit_string.lines().next().unwrap_or("?")
+            )
+        } else {
+            panic!("Can not get git commit: {}", commit.unwrap_err());
+        }
+    } else {
+        panic!("Can not get git branch: {}", branch.unwrap_err());
+    }
+}
+
 fn main() {
     // Don't rebuild miri when nothing changed.
     println!("cargo:rerun-if-changed=build.rs");
@@ -7,4 +30,5 @@ fn main() {
     println!("cargo:rustc-env=TARGET={target}");
     // Allow some cfgs.
     println!("cargo::rustc-check-cfg=cfg(bootstrap)");
+    println!("cargo:rustc-env=GIT_HASH={}", get_git_hash());
 }
